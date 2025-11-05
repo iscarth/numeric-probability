334ed82482b7caea5b9e1a51d5d10978ef47dcef
iscarth <156034918+iscarth@users.noreply.github.com>
2025-11-05 21:59:58 +0000

    Updated main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np 
+import scipy.integrate as spi 
+
+#---Example kernal functions---
+
+# this example shows a polynomial function
+def kernel_polynomial(x):
+    return x**3 + x
+
+# This example represents the normal distribution
+def kernel_normal(x):
+    return np.exp(-x**2)
+
+#---End of example kernal functions---
+
+
+#---Helpers for transformations of kernal fxn to probability fxn ---
+def normalize_kernel(kernel, lower_bound, upper_bound):
+    (integral, abs_error) = spi.quad(kernel, lower_bound, upper_bound)
+    def normalized_density(x):
+        return kernel(x)/ integral
+    return normalized_density
+
+def main():
+    polynomial_constant = spi.quad(kernel, lower_bound, upper_bound)
+    (integral, abs_error) = polynomial_constant
+    print(integral)
+
+if __name__ == "__main__":
+    main()
